db916e5a6932bf2137d6dbe008921c9d777e043a
KurtE <kurte@rockisland.com>
2021-12-12 07:40:37 -0800

    Media Present on BUILTIN_SDCARD
    Found the Media Present when card pulled out on T4.1 was taking about 3 seconds to complete.
    
    So this version tries to use the DAT3 pin on the SDCard to detect when the card is inserted.  Only if this goes high do we then try to initialize the SDCard again.
    
    there is an elapsedMicros that I use after this to try for up to a few seconds after this, before I set DAT3 back to IO pin... This allows the SDCard time to initialize...

--- src/SD.cpp
+++ src/SD.cpp
@@ -25,6 +25,17 @@
 
 SDClass SD;
 
+// defines for SD card insertion, mostly specific to SD BUILTIN
+enum {CPP_PRESENT=0x80, CPP_CD_ACTIVE=0x40, CPP_CD_DETECTED=0x20};
+#if defined(ARDUINO_TEENSY41)
+static const uint8_t _SD_DAT3=46;
+#elif defined(ARDUINO_TEENSY40) || defined(ARDUINO_TEENSY_MICROMOD)
+static const uint8_t _SD_DAT3=38;
+#elif defined(ARDUINO_TEENSY35) || defined(ARDUINO_TEENSY36)
+static const uint8_t _SD_DAT3=62;
+#endif
+
+
 #ifdef __arm__
 void SDClass::dateTime(uint16_t *date, uint16_t *time)
 {
@@ -40,6 +51,31 @@ void SDClass::dateTime(uint16_t *date, uint16_t *time)
 	}
 }
 #endif
+
+bool SDClass::begin(uint8_t csPin) {
+	csPin_ = csPin; // remember which one passed in. 
+#ifdef __arm__
+	FsDateTime::setCallback(dateTime);
+#endif
+#ifdef BUILTIN_SDCARD
+	if (csPin == BUILTIN_SDCARD) {
+		bool ret = sdfs.begin(SdioConfig(FIFO_SDIO));
+		if (!ret) {
+			pinMode(_SD_DAT3, INPUT_PULLDOWN);
+			cardPreviouslyPresent = CPP_CD_ACTIVE;
+		}
+		else cardPreviouslyPresent = 0;
+		return ret;
+	}
+#endif
+	if (csPin < NUM_DIGITAL_PINS) {
+		bool ret = sdfs.begin(SdSpiConfig(csPin, SHARED_SPI, SD_SCK_MHZ(25)));
+		cardPreviouslyPresent = ret ? CPP_PRESENT : 0;
+		return ret;
+	}
+	return false;
+}
+
 
 bool SDClass::format(int type, char progressChar, Print& pr)
 {
@@ -71,11 +107,18 @@ bool SDClass::format(int type, char progressChar, Print& pr)
 
 bool SDClass::mediaPresent()
 {
+	#ifdef BUILTIN_SDCARD 
+	// BUGBUG could/should be member variable
+	static elapsedMillis em_cd_detected;
+	#define MAX_CD_DETECTED_TEST 5000
+	#endif
 	//Serial.print("mediaPresent: ");
 	bool ret;
+	//elapsedMicros em = 0;
 	SdCard *card = sdfs.card();
+	//Serial.printf("sdfs.card() %u %u\n", (uint32_t)card, (uint32_t)em);
 	if (card) {
-		if (cardPreviouslyPresent) {
+		if (cardPreviouslyPresent == CPP_PRESENT) {
 			#ifdef BUILTIN_SDCARD
 			uint32_t s = card->status();
 			#else
@@ -91,22 +134,56 @@ bool SDClass::mediaPresent()
 				// bits 12:9 are card state, which should
 				// normally be 101 = data transfer mode
 				//Serial.print("status=offline");
+				cardPreviouslyPresent = 0;
+				#ifdef BUILTIN_SDCARD
+				if (csPin_ == BUILTIN_SDCARD) {
+					// setup the 
+					pinMode(_SD_DAT3, INPUT_PULLDOWN);
+					cardPreviouslyPresent = CPP_CD_ACTIVE;
+				} 
+			    #endif
 				ret = false;
 			} else {
 				//Serial.print("status=present");
 				ret = true;
 			}
 		} else {
+			#ifdef BUILTIN_SDCARD
+			if (csPin_ == BUILTIN_SDCARD) {
+				if (cardPreviouslyPresent == CPP_CD_DETECTED) {
+					// if we previously detected DAT3 and not card ready then 
+					// try for awhile to see if it will start
+					ret = sdfs.restart();
+					if (ret) cardPreviouslyPresent = CPP_PRESENT; 
+					else {
+						if (em_cd_detected >= MAX_CD_DETECTED_TEST) {
+							pinMode(_SD_DAT3, INPUT_PULLDOWN);
+							cardPreviouslyPresent = CPP_CD_ACTIVE;
+						}
+					}
+				}
+
+				else if ((cardPreviouslyPresent == CPP_CD_ACTIVE) && !digitalReadFast(_SD_DAT3)) 
+					ret = false;
+				else {
+					ret = sdfs.restart();
+					cardPreviouslyPresent = ret? CPP_PRESENT : CPP_CD_DETECTED;
+				}
+			} else 
+			#endif
+			{
 			// TODO: need a quick test, only call begin if likely present
-			ret = sdfs.restart();
+				ret = sdfs.restart();
+				if (ret) cardPreviouslyPresent = CPP_PRESENT;
 			//Serial.print(ret ? "begin ok" : "begin nope");
+			}
 		}
 	} else {
 		//Serial.print("no card");
 		ret = false;
 	}
 	//Serial.println();
-	cardPreviouslyPresent = ret;
+	//cardPreviouslyPresent = ret;
 	return ret;
 }
 
